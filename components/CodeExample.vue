7701120e04ef593752448800742e21ac0f659749
eeee0717 <chentao020717Work@outlook.com>
2024-11-09 15:03:37 +0800

    feat: finish content

---
+++ components/CodeExample.vue
@@ -0,0 +1,171 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="480px"
+    height="510px" viewBox="0 20 520 510" >
+    <defs />
+    <g>
+      <path d="M 305 431 L 305 399.24" fill="none" stroke="#b85450" stroke-width="2" stroke-miterlimit="10"
+        pointer-events="stroke" />
+      <path d="M 305 393.24 L 309 401.24 L 305 399.24 L 301 401.24 Z" fill="#b85450" stroke="#b85450" stroke-width="2"
+        stroke-miterlimit="10" pointer-events="all" />
+      <rect x="270" y="431" width="70" height="30" rx="4.5" ry="4.5" fill="#f8cecc" stroke="#b85450" stroke-width="2"
+        pointer-events="all" />
+      <g fill="rgb(0, 0, 0)" font-family="Comic Sans MS" text-anchor="middle" font-size="12px"><text x="304.5"
+          y="450.5">学习C#</text></g>
+      <path d="M 363 351 L 393.34 339.02" fill="none" stroke="#d79b00" stroke-width="2" stroke-miterlimit="10"
+        pointer-events="stroke" />
+      <path d="M 398.92 336.82 L 392.95 343.48 L 393.34 339.02 L 390.01 336.04 Z" fill="#d79b00" stroke="#d79b00"
+        stroke-width="2" stroke-miterlimit="10" pointer-events="all" />
+      <path d="M 363 391 L 392.91 396.75" fill="none" stroke="#d79b00" stroke-width="2" stroke-miterlimit="10"
+        pointer-events="stroke" />
+      <path d="M 398.8 397.89 L 390.19 400.3 L 392.91 396.75 L 391.7 392.45 Z" fill="#d79b00" stroke="#d79b00"
+        stroke-width="2" stroke-miterlimit="10" pointer-events="all" />
+      <path d="M 247 351 L 219.16 347.13" fill="none" stroke="#d79b00" stroke-width="2" stroke-miterlimit="10"
+        pointer-events="stroke" />
+      <path d="M 213.21 346.31 L 221.69 343.45 L 219.16 347.13 L 220.59 351.37 Z" fill="#d79b00" stroke="#d79b00"
+        stroke-width="2" stroke-miterlimit="10" pointer-events="all" />
+      <path d="M 247 391 L 218.6 402.83" fill="none" stroke="#d79b00" stroke-width="2" stroke-miterlimit="10"
+        pointer-events="stroke" />
+      <path d="M 213.06 405.14 L 218.91 398.37 L 218.6 402.83 L 221.99 405.76 Z" fill="#d79b00" stroke="#d79b00"
+        stroke-width="2" stroke-miterlimit="10" pointer-events="all" />
+      <path d="M 305 351 L 305 299.24" fill="none" stroke="#d79b00" stroke-width="2" stroke-miterlimit="10"
+        pointer-events="stroke" />
+      <path d="M 305 293.24 L 309 301.24 L 305 299.24 L 301 301.24 Z" fill="#d79b00" stroke="#d79b00" stroke-width="2"
+        stroke-miterlimit="10" pointer-events="all" />
+      <rect x="247" y="351" width="116" height="40" fill="#ffe6cc" stroke="#d79b00" stroke-width="2"
+        pointer-events="all" />
+      <g fill="rgb(0, 0, 0)" font-family="Comic Sans MS" text-anchor="middle" font-size="12px"><text x="304.5"
+          y="375.5">打开B站搜索视频</text></g>
+      <rect x="131" y="391" width="80" height="30" fill="#f5f5f5" stroke="#666666" stroke-width="2"
+        pointer-events="all" />
+      <g fill="#333333" font-family="Comic Sans MS" text-anchor="middle" font-size="12px"><text x="170.5"
+          y="410.5">C#入门</text></g>
+      <rect x="131" y="331" width="80" height="30" fill="#f5f5f5" stroke="#666666" stroke-width="2"
+        pointer-events="all" />
+      <g fill="#333333" font-family="Comic Sans MS" text-anchor="middle" font-size="12px"><text x="170.5"
+          y="350.5">C#零基础</text></g>
+      <rect x="401" y="391" width="80" height="30" fill="#f5f5f5" stroke="#666666" stroke-width="2"
+        pointer-events="all" />
+      <g fill="#333333" font-family="Comic Sans MS" text-anchor="middle" font-size="12px"><text x="440.5"
+          y="410.5">WPF开发</text></g>
+      <rect x="401" y="321" width="80" height="30" fill="#f5f5f5" stroke="#666666" stroke-width="2"
+        pointer-events="all" />
+      <g fill="#333333" font-family="Comic Sans MS" text-anchor="middle" font-size="12px"><text x="440.5"
+          y="340.5">.NET开发</text></g>
+      <path d="M 363 251 L 392.83 247.07" fill="none" stroke="#6c8ebf" stroke-width="2" stroke-miterlimit="10"
+        pointer-events="stroke" />
+      <path d="M 398.78 246.29 L 391.37 251.3 L 392.83 247.07 L 390.33 243.37 Z" fill="#6c8ebf" stroke="#6c8ebf"
+        stroke-width="2" stroke-miterlimit="10" pointer-events="all" />
+      <path d="M 247 251 L 228.13 240.12" fill="none" stroke="#6c8ebf" stroke-width="2" stroke-miterlimit="10"
+        pointer-events="stroke" />
+      <path d="M 222.94 237.12 L 231.87 237.65 L 228.13 240.12 L 227.87 244.58 Z" fill="#6c8ebf" stroke="#6c8ebf"
+        stroke-width="2" stroke-miterlimit="10" pointer-events="all" />
+      <path d="M 247 291 L 218.94 298.8" fill="none" stroke="#6c8ebf" stroke-width="2" stroke-miterlimit="10"
+        pointer-events="stroke" />
+      <path d="M 213.15 300.4 L 219.79 294.41 L 218.94 298.8 L 221.93 302.11 Z" fill="#6c8ebf" stroke="#6c8ebf"
+        stroke-width="2" stroke-miterlimit="10" pointer-events="all" />
+      <path d="M 305 251 L 305 179.24" fill="none" stroke="#6c8ebf" stroke-width="2" stroke-miterlimit="10"
+        pointer-events="stroke" />
+      <path d="M 305 173.24 L 309 181.24 L 305 179.24 L 301 181.24 Z" fill="#6c8ebf" stroke="#6c8ebf" stroke-width="2"
+        stroke-miterlimit="10" pointer-events="all" />
+      <rect x="247" y="251" width="116" height="40" fill="#dae8fc" stroke="#6c8ebf" stroke-width="2"
+        pointer-events="all" />
+      <g fill="rgb(0, 0, 0)" font-family="Comic Sans MS" text-anchor="middle" font-size="12px"><text x="304.5"
+          y="275.5">安装开发环境</text></g>
+      <rect x="401" y="231" width="80" height="30" fill="#f5f5f5" stroke="#666666" stroke-width="2"
+        pointer-events="all" />
+      <g fill="#333333" font-family="Comic Sans MS" text-anchor="middle" font-size="12px"><text x="440.5"
+          y="250.5">Visual Studio</text></g>
+      <rect x="141" y="221" width="80" height="30" fill="#f5f5f5" stroke="#666666" stroke-width="2"
+        pointer-events="all" />
+      <g fill="#333333" font-family="Comic Sans MS" text-anchor="middle" font-size="12px"><text x="180.5"
+          y="240.5">WPF框架</text></g>
+      <rect x="151" y="291" width="60" height="20" fill="#f5f5f5" stroke="#666666" stroke-width="2"
+        pointer-events="all" />
+      <g fill="#333333" font-family="Comic Sans MS" text-anchor="middle" font-size="12px"><text x="180.5"
+          y="305.5">Git</text></g>
+      <path d="M 363 131 L 395.69 92.29" fill="none" stroke="#82b366" stroke-width="2" stroke-miterlimit="10"
+        pointer-events="stroke" />
+      <path d="M 399.56 87.71 L 397.45 96.4 L 395.69 92.29 L 391.34 91.24 Z" fill="#82b366" stroke="#82b366"
+        stroke-width="2" stroke-miterlimit="10" pointer-events="all" />
+      <path d="M 247 131 L 225.91 102.61" fill="none" stroke="#82b366" stroke-width="2" stroke-miterlimit="10"
+        pointer-events="stroke" />
+      <path d="M 222.33 97.79 L 230.31 101.83 L 225.91 102.61 L 223.89 106.6 Z" fill="#82b366" stroke="#82b366"
+        stroke-width="2" stroke-miterlimit="10" pointer-events="all" />
+      <path d="M 247 171 L 209.19 166.89" fill="none" stroke="#82b366" stroke-width="2" stroke-miterlimit="10"
+        pointer-events="stroke" />
+      <path d="M 203.22 166.24 L 211.61 163.13 L 209.19 166.89 L 210.74 171.08 Z" fill="#82b366" stroke="#82b366"
+        stroke-width="2" stroke-miterlimit="10" pointer-events="all" />
+      <path d="M 363 171 L 403.14 183.54" fill="none" stroke="#82b366" stroke-width="2" stroke-miterlimit="10"
+        pointer-events="stroke" />
+      <path d="M 408.87 185.33 L 400.04 186.76 L 403.14 183.54 L 402.42 179.13 Z" fill="#82b366" stroke="#82b366"
+        stroke-width="2" stroke-miterlimit="10" pointer-events="all" />
+      <path d="M 305 131 L 305 59.24" fill="none" stroke="#82b366" stroke-width="2" stroke-miterlimit="10"
+        pointer-events="stroke" />
+      <path d="M 305 53.24 L 309 61.24 L 305 59.24 L 301 61.24 Z" fill="#82b366" stroke="#82b366" stroke-width="2"
+        stroke-miterlimit="10" pointer-events="all" />
+      <rect x="247" y="131" width="116" height="40" fill="#d5e8d4" stroke="#82b366" stroke-width="2"
+        pointer-events="all" />
+      <g fill="rgb(0, 0, 0)" font-family="Comic Sans MS" text-anchor="middle" font-size="12px"><text x="304.5"
+          y="155.5">C#基础语法</text></g>
+      <path d="M 141 81 L 116.59 54.55" fill="none" stroke="#82b366" stroke-width="2" stroke-miterlimit="10"
+        pointer-events="stroke" />
+      <path d="M 112.52 50.14 L 120.88 53.31 L 116.59 54.55 L 115 58.73 Z" fill="#82b366" stroke="#82b366"
+        stroke-width="2" stroke-miterlimit="10" pointer-events="all" />
+      <path d="M 141 103.5 L 119.21 105.32" fill="none" stroke="#82b366" stroke-width="2" stroke-miterlimit="10"
+        pointer-events="stroke" />
+      <path d="M 113.23 105.81 L 120.87 101.16 L 119.21 105.32 L 121.53 109.14 Z" fill="#82b366" stroke="#82b366"
+        stroke-width="2" stroke-miterlimit="10" pointer-events="all" />
+      <rect x="141" y="81" width="80" height="30" fill="#f5f5f5" stroke="#666666" stroke-width="2"
+        pointer-events="all" />
+      <g fill="#333333" font-family="Comic Sans MS" text-anchor="middle" font-size="12px"><text x="180.5"
+          y="100.5">变量与常量</text></g>
+      <path d="M 121 151 L 99.12 154.65" fill="none" stroke="#82b366" stroke-width="2" stroke-miterlimit="10"
+        pointer-events="stroke" />
+      <path d="M 93.21 155.63 L 100.44 150.37 L 99.12 154.65 L 101.75 158.26 Z" fill="#82b366" stroke="#82b366"
+        stroke-width="2" stroke-miterlimit="10" pointer-events="all" />
+      <path d="M 121 181 L 97.33 200.73" fill="none" stroke="#82b366" stroke-width="2" stroke-miterlimit="10"
+        pointer-events="stroke" />
+      <path d="M 92.72 204.57 L 96.3 196.37 L 97.33 200.73 L 101.42 202.52 Z" fill="#82b366" stroke="#82b366"
+        stroke-width="2" stroke-miterlimit="10" pointer-events="all" />
+      <rect x="121" y="151" width="80" height="30" fill="#f5f5f5" stroke="#666666" stroke-width="2"
+        pointer-events="all" />
+      <g fill="#333333" font-family="Comic Sans MS" text-anchor="middle" font-size="12px"><text x="160.5"
+          y="170.5">结构体与类</text></g>
+      <rect x="401" y="71" width="80" height="30" fill="#f5f5f5" stroke="#666666" stroke-width="2"
+        pointer-events="all" />
+      <g fill="#333333" font-family="Comic Sans MS" text-anchor="middle" font-size="12px"><text x="440.5"
+          y="90.5">流程控制</text></g>
+      <rect x="411" y="171" width="80" height="30" fill="#f5f5f5" stroke="#666666" stroke-width="2"
+        pointer-events="all" />
+      <g fill="#333333" font-family="Comic Sans MS" text-anchor="middle" font-size="12px"><text x="450.5"
+          y="190.5">基本变量</text></g>
+      <path d="M 363 31 L 501 31 L 501 491 L 305 491 L 305 469.24" fill="none" stroke="#9673a6" stroke-width="2"
+        stroke-miterlimit="10" pointer-events="stroke" />
+      <path d="M 305 463.24 L 309 471.24 L 305 469.24 L 301 471.24 Z" fill="#9673a6" stroke="#9673a6" stroke-width="2"
+        stroke-miterlimit="10" pointer-events="all" />
+      <rect x="247" y="11" width="116" height="40" fill="#e1d5e7" stroke="#9673a6" stroke-width="2"
+        pointer-events="all" />
+      <g fill="rgb(0, 0, 0)" font-family="Comic Sans MS" text-anchor="middle" font-size="12px"><text x="304.5"
+          y="35.5">开始动手做东西</text></g>
+      <rect x="31" y="41" width="80" height="30" fill="#f5f5f5" stroke="#666666" stroke-width="2"
+        pointer-events="all" />
+      <g fill="#333333" font-family="Comic Sans MS" text-anchor="middle" font-size="12px"><text x="70.5"
+          y="60.5">var</text></g>
+      <rect x="31" y="91" width="80" height="30" fill="#f5f5f5" stroke="#666666" stroke-width="2"
+        pointer-events="all" />
+      <g fill="#333333" font-family="Comic Sans MS" text-anchor="middle" font-size="12px"><text x="70.5"
+          y="110.5">const</text></g>
+      <rect x="11" y="141" width="80" height="30" fill="#f5f5f5" stroke="#666666" stroke-width="2"
+        pointer-events="all" />
+      <g fill="#333333" font-family="Comic Sans MS" text-anchor="middle" font-size="12px"><text x="50.5"
+          y="160.5">OOP</text></g>
+      <rect x="11" y="191" width="80" height="30" fill="#f5f5f5" stroke="#666666" stroke-width="2"
+        pointer-events="all" />
+      <g fill="#333333" font-family="Comic Sans MS" text-anchor="middle" font-size="12px"><text x="50.5"
+          y="210.5">接口</text></g>
+    </g>
+  </svg>
+</template>
--- slides.md
+++ slides.md
@@ -7,16 +7,15 @@ transition: fade-out
 mdc: true
 glowSeed: 4
 title: ChenTao's Journey
-description: ChenTao's journey of development. 
+description: ChenTao's journey of development.
 fonts:
-  sans: LXGW WenKai Mono TC 
+  sans: LXGW WenKai Mono TC
 ---
 
 ChenTao's Roads to Development
 
 ---
 layout: intro
-class: pl-30
 glowSeed: 14
 ---
 
@@ -38,12 +37,14 @@ glowSeed: 14
   <div><a href="https://space.bilibili.com/52423255" target="_blank" class="border-none! font-300" ws-nowrap>槑囿脑袋</a></div>
 </div>
 
-<img src="/me.jpg"  absolute top-36 right-35 w-40 rounded-full />
----
+<img src="/me.jpg"  absolute top-30 right-45 w-40 rounded-full />
+<img src="/GitHub.png" absolute top-75 right-8 w-110  />
 
+---
 
 # Projects Gallery
 <ListProjects />
+
 ---
 layout: cover
 title: Anthony's Roads to Open Source
@@ -69,3 +70,178 @@ class: pl20
     </div>
   </div>
 </div>
+
+---
+
+# 学习编程的例子
+
+<div mt-4 />
+
+<div grid="~ cols-[4fr_2fr] gap-2">
+
+<v-clicks>
+
+- 你今天打算<span text-red>学习C#</span>
+- 打开B站搜索<span text-orange>C#教学</span>；
+- 花了几个小时安装好了<br><span text-green>Visual Studio和.NET框架</span>；
+- 花了一个星期学习C#基础语法：<br><span text-violet>变量</span>，<span text-yellow>基本类型</span>，<span text-blue>流程控制</span>，<span text-teal>结构体与类</span> 等等；
+- 一段时间后<span text-fuchsia>看</span>完了视频...
+- 准备动手做一个东西的时候发现...
+- 好像什么都学了但又好像什么也<span text-amber>没学会</span>...
+- 于是你又重新换一个<span text-rose>C#视频</span>学习...
+
+</v-clicks>
+<div v-click>
+  <CodeExample scale-80 />
+</div>
+</div>
+---
+glowSeed: 10
+---
+
+# 如何学习「编程」?
+
+<div v-click>以<span v-mark.orange="2">项目</span>为目标可以很好的帮助我们学习「编程」</div>
+<div v-click/>
+<div flex="~ col gap-7" py10>
+
+<div v-click><div flex="~ inline" text-blue mr2 px2 rounded bg-blue:10>需求</div>从自己的需求出发，对问题有更深入的理解</div>
+<div v-click><div flex="~ inline" text-rose mr2 px2 rounded bg-rose:10>动力</div>解决自己的问题，同时或许也可以帮助到其他人</div>
+<div v-click><div flex="~ inline" text-yellow mr2 px2 rounded bg-yellow:10>验证</div>你的需求很可能也是别人的需求，通过社区验证你的想法</div>
+<div v-click><div flex="~ inline" text-green mr2 px2 rounded bg-green:10>迭代</div>社区讨论与贡献，完善设计</div>
+
+</div>
+
+<div absolute left-150 top-20>
+  <div
+    absolute w-50 h-50 left-0 top-28 border="~ blue rounded-full"
+    bg-blue:20 text-xl text-blue flex="~ items-center justify-center"
+    v-click="3"
+  >
+    <div :class="{pr15: $clicks>2, pt8: $clicks>3}" transition-all duration-400>你擅长的</div>
+  </div>
+  <div
+    absolute w-50 h-50 left-34 top-28 border="~ rose rounded-full"
+    bg-rose:20 text-xl text-rose flex="~ items-center justify-center"
+    v-click="4"
+  >
+    <div pl15 :class="{pt8: $clicks>3}" transition-all duration-400>你享受的</div>
+  </div>
+  <div
+    absolute w-50 h-50 left-17 top-0 border="~ amber rounded-full"
+    bg-amber:20 text-xl text-amber flex="~ items-center justify-center"
+    v-click="5"
+  >
+    <div pb10>大家需要的</div>
+  </div>
+  <Arrow v-click="6" x1="160" y1="340" x2="170" y2="175" color-green  />
+  <div
+    v-click="6"
+    absolute top-85 left-5 ws-nowrap
+    border="~ green rounded-full" px3 py1
+    bg-green:20 text-xl text-green
+  >实用且可持续发展的开源项目</div>
+</div>
+
+
+---
+glow: right
+---
+
+# 开源工具发想与实现流程
+
+<div flex="~ col gap-1" py5>
+  <div v-click flex="~ items-center gap-2"><div text-2xl i-ph-lightbulb-duotone text-yellow /> 理解问题，考虑可能的解决方案</div>
+  <div v-click i-ph-arrow-down op25 ml-1 text-sm />
+  <div v-after flex="~ items-center gap-2"><div text-2xl i-ph-magnifying-glass-duotone text-blue /> 寻找市面上已经存在的库</div>
+  <div v-click absolute left-76 top-40 bg-blue:10 border="~ blue/50 rounded-lg" p2 py1>
+    <div text-sm text-blue2>
+      参与贡献，提出建议
+    </div>
+  </div>
+  <div v-click i-ph-arrow-down op25 ml-1 text-sm />
+  <div flex="~ items-center gap-2">
+    <div text-2xl i-ph-code-duotone text-green v-after />
+    <span v-after>本地实现 MVP；验证方案可行性；保持接口解耦的想法进行设计；</span>
+    <span v-click text-yellow font-bold flex="~ gap-1 items-center" ml--2>及时回归<div i-ph-arrow-square-out-duotone /></span>
+  </div>
+  <div v-click absolute left-134 top-32 bg-yellow:10 border="~ yellow/50 rounded-lg" p2 w-54>
+    <div text-sm text-yellow2>
+      避免「深度优先陷阱」，完成必要的功能后及时回归主线任务。日后再继续深入细节。
+    </div>
+  </div>
+  <div v-click i-ph-arrow-down op25 ml-1 text-sm />
+  <div v-after flex="~ items-center gap-2"><div text-2xl i-ph-books-duotone text-teal /> 抽离实现成为库；添加简要的文档与动机；开源仓库发布包</div>
+  <div v-click i-ph-arrow-down op25 ml-1 text-sm />
+  <div v-after flex="~ items-center gap-2"><div text-2xl i-ph-megaphone-duotone text-rose /> 适当的宣传；搜集社区反馈</div>
+  <div v-click absolute left-80 top-77 bg-rose:10 border="~ rose/50 rounded-lg" p2 py1>
+    <div text-sm text-rose2>
+      鼓励总结成技术博客
+    </div>
+  </div>
+  <div v-click i-ph-arrow-down op25 ml-1 text-sm />
+  <div v-after flex="~ items-center gap-2"><div text-2xl i-ph-chats-circle-duotone text-violet /> 交由社区验证</div>
+    <div v-click flex="~ items-center gap-2" pl-4 mt4><div i-ph-arrow-elbow-down-right op25 ml-1 text-sm /><div text-2xl i-ph-seal-question-duotone text-gray /> 无人问津：解决了自己的问题、总结了解决方案，也是不错的收获</div>
+    <div v-click flex="~ items-center gap-2" pl-4 mt4><div i-ph-arrow-elbow-down-right op25 ml-1 text-sm /><div text-2xl i-ph-treasure-chest-duotone text-orange /> 诶好像还不错：社区参与讨论、报错、贡献等；帮助到了更多也帮到了自己</div>
+</div>
+
+
+---
+glow: bottom
+---
+
+# 对开源的期待
+
+<div grid="~ cols-3 gap-2" py4>
+  <div v-click flex="~ col gap-1" p4 rounded bg-violet:15 text-violet1>
+    <div text-2xl i-ph-gift-duotone text-violet mb2 />
+    <div>开源是一种礼物</div>
+    <div text-xs op50>Open Source is a Gift</div>
+  </div>
+
+  <div v-click flex="~ col gap-1" p4 rounded bg-orange:15 text-orange1>
+    <div text-2xl i-ph-target-duotone text-orange mb2 />
+    <div>互惠不是目的</div>
+    <div text-xs op50>Reciprocity is not the Goal</div>
+  </div>
+
+  <div v-click flex="~ col gap-1" p4 rounded bg-rose:15 text-rose1>
+    <div text-2xl i-ph-hand-heart-duotone text-rose mb2 />
+    <div>享受分享和合作的过程</div>
+    <div text-xs op50>Enjoy the Sharing and Collaboration</div>
+  </div>
+
+  <div v-click flex="~ col gap-1" p4 rounded bg-green:15 text-green1>
+    <div text-2xl i-ph-hand-waving-duotone text-green mb2 />
+    <div>开源不只有代码</div>
+    <div text-xs op50>Open Source is more than Code</div>
+  </div>
+
+  <div v-click flex="~ col gap-1" p4 rounded bg-blue:15 text-blue1>
+    <div text-2xl i-ph-users-three-duotone text-blue mb2 />
+    <div>开源的核心是社区</div>
+    <div text-xs op50>The Core of Open Source is Community</div>
+  </div>
+</div>
+
+<div absolute bottom-10 v-click>
+<div op75 mb1>文章推荐</div>
+
+- [Open-Source is a Gift](https://www.redotheweb.com/2011/11/13/open-source-is-a-gift.html)<span op50 text-sm> - François Zaninotto</span>
+- [The open source gift exchange](https://world.hey.com/dhh/the-open-source-gift-exchange-2171e0f0)<span op50 text-sm> - David Heinemeier Hansson</span>
+- [开源的心理建设](https://antfu.me/posts/mental-health-oss-zh)<span op50 text-sm> - Anthony Fu</span>
+
+
+</div>
+
+
+---
+layout: intro
+class: text-center pb-5
+glowX: 50
+glowY: 120
+---
+
+# Happy Coding!
+
+<p pt-2 op50>谢谢！有关编程的问题欢迎一起交流讨论！</p>
