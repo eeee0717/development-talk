1348ea77b190f08cfe678bb8e2a143c58ad15e71
eeee0717 <chentao020717Work@outlook.com>
2024-11-10 11:16:51 +0800

    feat: let video and picture display well

--- components/ListProjects.vue
+++ components/ListProjects.vue
@@ -1,31 +1,31 @@
-
 <template>
-  <div  grid="~ cols-3 rows-2 gap-4">    
-    <div >
-      <video h-150px overflow="hidden" autoplay loop muted playsinline src="/projects/subtitle.mp4"  />
+  <div grid="~ cols-3 rows-2 gap-4">
+    <div>
+      <video  overflow="hidden" autoplay loop muted playsinline src="/projects/subtitle2.mp4" />
       <h4 class="flex justify-center">Subtitle-Translator-CLI</h4>
     </div>
-    <div >
-      <video h-150px overflow="hidden" autoplay loop muted playsinline src="/projects/podwise.mp4"  />
+    <div>
+      <video  overflow="hidden" autoplay loop muted playsinline src="/projects/podwise2.mp4" />
       <h4 class="flex justify-center">Podwise-AI</h4>
-    </div>   
-     <div >
-      <img  src="/projects/project1.png"  />
-      <h4 class="flex justify-center">核电站管道故障检测系统</h4>
-    </div>
-    <div >
-      <img   src="/projects/project1.png"  />
-      <h4 class="flex justify-center">医疗标签打印系统</h4>
-    </div>
-    <div >
-      <img   src="/projects/project1.png"  />
-      <h4 class="flex justify-center">精密车床控制系统</h4>
     </div>
     <div>
+      <video  overflow="hidden" autoplay loop muted playsinline src="/projects/mine2.mp4" />
+      <h4 class="flex justify-center">扫雷</h4>
+    </div>
+    <div>
+      <img src="/projects/project1.png" />
+      <h4 class="flex justify-center">核电站管道故障检测系统</h4>
+    </div>
+    <div>
+      <img src="/projects/toolkit.png" />
+      <h4 class="flex justify-center">Toolkit</h4>
+    </div>
+
+    <div>
       <a target="_blank" href="https://www.bilibili.com/video/BV1iz4y1E7sC/?vd_source=e69adc4cbcbf14d298fc66f0ae53c5c8">
-        <img  src="/projects/project2.png"  />
+        <img src="/projects/project2.png" />
         <h4 class="flex justify-center">"知音"智能家居系统</h4>
       </a>
     </div>
-</div>
+  </div>
 </template>
